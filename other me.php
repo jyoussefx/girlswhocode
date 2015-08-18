880c2dc36f1d77236c2cdecccd763d45c62cbff2
alexxa99 <alexa.mencia99@gmail.com>
2015-08-18 14:08:03 -0400

    initial commit

---
+++ other me.php
@@ -0,0 +1,16 @@
+</html>
+<head>
+<title> About Alexa</title>
+</head>
+<body bgcolor="#FFA07A">
+<ul>
+<li> 16 years old </li>
+<li> Girls Who Code</li>
+<li>Swimming and Tennis</li>
+<li> Local Feminist </li>
+<li> Concert Goer</li>
+
+
+<p><small><em>Copyright copy; Alexa Foundation</em></small></p>
+</body>
+</html>
