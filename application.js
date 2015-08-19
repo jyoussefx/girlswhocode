cf075e0811ddca1ca3e65a6aced7632134b56c41
JYoussefx <joannayoussefx@gmail.com>
2015-08-19 14:38:21 -0400

    edited SIGN UP code, when you enter a username and fulname it adds it to the dictionary still working on the other variables

--- application.js
+++ application.js
@@ -1,11 +1,26 @@
+
+var userFullName={};
+var userPIN={}
+var userEmail={};
+var userPhone={};
+
+
 $(document).ready(function(){ 
 
          //when the <submit> button is clicked
          $('button').click(function(){
 
          //store the user's entry in a variable
-         var enteredName = document.getElementById('enteredFormName').value;
+         var enteredName= document.getElementById('fullName').value;
 		  var enteredPIN = document.getElementById('PIN').value;
+		   var entereduserName= document.getElementById('userName').value;
+		  var enteredEmail = document.getElementById('Email').value;
+		  var enteredNum = document.getElementById('phoneNumber').value;
+
+		  userFullName[entereduserName] = enteredName
+		  
+		  console.log(userFullName);
+		  
          //OR var enteredName = $('#enteredFormName').val();
          //update the html in the second form to show the user's entered name
         // $('#fixedFormName').val(enteredName);
